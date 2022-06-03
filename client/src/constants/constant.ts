8dead2c11c9f8cb941ca0e8c82f145bb4c039005
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-03 23:42:33 +0900

    :recycle: 필요한 상수 별도 파일로 분리

---
+++ client/src/constants/constant.ts
@@ -0,0 +1,1 @@
+export const BACK_BASE_URL = "http://localhost:5000";
---
+++ client/src/constants/socket.ts
@@ -0,0 +1,9 @@
+export const CREATE_ROOM = "CREATE_ROOM";
+export const CREATE_SUCCESS = "CREATE_SUCCESS";
+export const JOIN_ROOM = "JOIN_ROOM";
+export const ENTER_ONE_USER = "ENTER_ONE_USER";
+export const ENTER_OTHER_USER = "ENTER_OTHER_USER";
+export const EXIST_ROOM_ERROR = "EXIST_ROOM_ERROR";
+
+export const DRAWING = "DRAWING";
+export const OTHER_DRAWING = "OTHER_DRAWING";
