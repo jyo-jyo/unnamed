14427ef1ab4b00050c015d70074e9494f05b3add
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-03 23:41:14 +0900

    :sparkles: 방 생성을 위한 server socket 설정
    방 생성을 위한 server socket 이벤트 등록

--- server/src/loader/socket.ts
+++ server/src/loader/socket.ts
@@ -1,8 +1,33 @@
 import { Socket, Server } from "socket.io";
 
-const FRONT_BASE_URL = "localhost";
+import { FRONT_BASE_URL } from "../constants/constant";
+import {
+  CREATE_ROOM,
+  JOIN_ROOM,
+  ENTER_ONE_USER,
+  ENTER_OTHER_USER,
+  DRAWING,
+  OTHER_DRAWING,
+  EXIST_ROOM_ERROR,
+  CREATE_SUCCESS,
+} from "../constants/socket";
+
+interface RoomType {
+  [roomCode: string]: {
+    hostId: string | null;
+    users: string[];
+  };
+}
+const createRoomCode = (rooms: RoomType) => {
+  while (true) {
+    const code = Math.random().toString(16).substr(2, 5);
+    if (!(code in rooms)) return code;
+  }
+};
 
 const socketLoader = (server: any, app: any): any => {
+  const rooms = <RoomType>{};
+
   const io = new Server(server, {
     cors: {
       origin: FRONT_BASE_URL,
@@ -13,6 +38,16 @@ const socketLoader = (server: any, app: any): any => {
 
   io.on("connection", (socket: Socket): void => {
     console.log("socket connection!!", socket.id);
+
+    socket.on(CREATE_ROOM, () => {
+      const roomCode = createRoomCode(rooms);
+      rooms[roomCode] = {
+        hostId: null,
+        users: [],
+      };
+      socket.emit(CREATE_SUCCESS, roomCode);
+    });
+
     socket.on("disconnect", () => {
       console.log("disconnect socket!!" + socket.id);
     });
