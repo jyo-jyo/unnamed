6ccd000590071d73dfc1625d360e293ee2f4d3ad
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-06 22:16:50 +0900

    :sparkles: 공유 그림판 기능 구현
    같은 방에 들어와 있는 사용자는 공유 그림판을 이용할 수 있다.
    추후 개선사항
    - 동시 그리기 지원
    - 순서 기능 지원

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,21 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 import { BoardContainer } from "./Board.style";
+import Socket from "../socket";
 
 const Board = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef<CanvasRenderingContext2D>();
+  const [isMyTurn, setIsMyTurn] = useState<boolean>(true);
   const [isDrawing, setIsDrawing] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (!canvasRef.current) return;
-    const canvas = canvasRef.current;
-    canvas.height = canvas.parentElement?.offsetHeight ?? 500;
-    canvas.width = canvas.parentElement?.offsetWidth ?? 500;
-    const context = canvas.getContext("2d");
-    if (context) {
-      contextRef.current = context;
-    }
-  }, []);
+  const socket = useRef<any>();
+  const roomCode = useParams().roomCode?.slice(1);
 
   const startDrawing = () => {
     setIsDrawing(true);
@@ -25,10 +19,17 @@ const Board = () => {
     setIsDrawing(false);
   };
 
-  const drawing = ({ nativeEvent }: { nativeEvent: MouseEvent }) => {
+  const drawing = ({
+    isDrawing,
+    offsetX,
+    offsetY,
+  }: {
+    isDrawing: boolean;
+    offsetX: number;
+    offsetY: number;
+  }) => {
     const ctx = contextRef.current;
     if (!ctx) return;
-    const { offsetX, offsetY } = nativeEvent;
     if (!isDrawing) {
       ctx.beginPath();
       ctx.moveTo(offsetX, offsetY);
@@ -37,17 +38,49 @@ const Board = () => {
       ctx.stroke();
     }
   };
+  useEffect(() => {
+    if (socket.current) return;
+    socket.current = Socket.drawing({
+      otherDrawing: drawing,
+    });
+  }, []);
 
-  return (
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    canvas.height = canvas.parentElement?.offsetHeight ?? 250;
+    canvas.width = canvas.parentElement?.offsetWidth ?? 250;
+    const context = canvas.getContext("2d");
+    if (context) {
+      contextRef.current = context;
+    }
+    return () => {
+      socket.current?.disconnecting();
+    };
+  }, [canvasRef.current]);
+
+  return socket.current ? (
     <BoardContainer>
       <canvas
         ref={canvasRef}
         onMouseDown={startDrawing}
-        onMouseMove={drawing}
+        onMouseMove={({ nativeEvent }: { nativeEvent: MouseEvent }) =>
+          isMyTurn &&
+          socket.current.drawing({
+            roomCode,
+            drawingData: {
+              isDrawing,
+              offsetX: nativeEvent.offsetX,
+              offsetY: nativeEvent.offsetY,
+            },
+          })
+        }
         onMouseUp={stopDrawing}
         onMouseLeave={stopDrawing}
       ></canvas>
     </BoardContainer>
+  ) : (
+    <></>
   );
 };
 
