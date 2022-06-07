2f43a1f3d914cf97a45ec44057121c8132488768
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-07 19:02:02 +0900

    :lipstick: 조회된 방 정보 css 적용
    기본 css 적용 추후 수정 요망

--- client/src/components/Lobby/Room.style.ts
+++ client/src/components/Lobby/Room.style.ts
@@ -2,10 +2,31 @@ import styled from "styled-components";
 import { COLOR } from "../../constants/style";
 
 export const RoomContainer = styled.div<{ isAccessible: boolean }>`
+  width: 250px;
+  height: 80px;
+
+  padding: 10px;
+  margin: 5px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  color: ${({ isAccessible }) => (isAccessible ? "black" : COLOR.disabled)};
+  border: solid 1px
+    ${({ isAccessible }) => (isAccessible ? "black" : COLOR.disabled)};
+  hr {
+    width: 100%;
+  }
 `;
 
 export const RoomTitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const RoomInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 `;
