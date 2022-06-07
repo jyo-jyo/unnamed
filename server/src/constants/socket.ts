730d433bf58aaa6777073536949912880de7d502
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-07 18:51:21 +0900

    :sparkles: socket 이벤트 관련 상수 추가

--- server/src/constants/socket.ts
+++ server/src/constants/socket.ts
@@ -1,9 +1,17 @@
+// create room
 export const CREATE_ROOM = "CREATE_ROOM";
 export const CREATE_SUCCESS = "CREATE_SUCCESS";
+export const ROOM_LIST = "ROOM_LIST";
+
+// join room
 export const JOIN_ROOM = "JOIN_ROOM";
 export const ENTER_ONE_USER = "ENTER_ONE_USER";
 export const ENTER_OTHER_USER = "ENTER_OTHER_USER";
 export const EXIST_ROOM_ERROR = "EXIST_ROOM_ERROR";
 
+// drawing
 export const DRAWING = "DRAWING";
 export const OTHER_DRAWING = "OTHER_DRAWING";
+
+// game
+export const NEXT_TURN = "NEXT_TURN";
