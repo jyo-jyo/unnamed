27873f0008b9478faf97cf8d5834e878ec144422
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-08 19:02:40 +0900

    :sparkles: 방 생성 정보 입력 모달 구현
    방 생성 시 방 정보를 입력할 수 있는 기능을 구현

---
+++ client/src/components/Lobby/CreateRoomModal.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CreateRoomModalContainer = styled.div`
+`;
+
+export const InputBox = styled.div`
+`;
+
+export const DropBoxInputBox = styled.div`
+`;
