195c6dce19921a5f7f748014b36bdd2b7ccc29ff
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-08 19:01:06 +0900

    :lipstick: DropBox 기본 css 적용

--- client/src/components/common/DropBox.tsx
+++ client/src/components/common/DropBox.tsx
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   DropBoxContainer,
   DropBoxButton,
   DropBoxItemBox,
   DropBoxItem,
 } from "./DropBox.style";
-const DropBox = (
-  options: [],
-  selected: string | undefined,
-  setSelected: Function
-) => {
+const DropBox = ({
+  options,
+  selected,
+  setSelected,
+}: {
+  options: number[] | string[];
+  selected: string | number | undefined;
+  setSelected: Function;
+}) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const isActive = useRef<boolean>(false);
+
   return (
-    <DropBoxContainer>
-      <DropBoxButton>{selected}</DropBoxButton>
+    <DropBoxContainer
+      onMouseOver={() => (isActive.current = true)}
+      onMouseLeave={() => (isActive.current = false)}
+    >
+      <DropBoxButton onClick={() => setIsOpen((prev) => !prev)}>
+        {selected}
+      </DropBoxButton>
       <DropBoxItemBox isOpen={isOpen}>
-        {options.map((option) => (
+        {options.map((option, index) => (
           <DropBoxItem
             onClick={() => {
               setSelected(option);
               setIsOpen(false);
             }}
+            isFirst={index === 0}
+            isLast={index === options.length - 1}
           >
             {option}
           </DropBoxItem>
