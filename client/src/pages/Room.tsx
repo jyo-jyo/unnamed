47ca3c287232619733c807932f8cf0c2ee4e23cd
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-10 22:56:15 +0900

    :sparkles: 방 정보 출력 기능 구현
    타입 지정 및 방 정보를 출력하는 기능을 구현

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,9 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
-import Board from "../components/Board";
+import { useNavigate, useParams } from "react-router-dom";
+import Board from "../components/Room/Board";
 import Socket from "../socket";
+
+interface RoomType {
+  hostId: string | null;
+  users: string[];
+  gameState: {
+    isPlaying: boolean;
+    currOrder: number;
+    currRound: number;
+  };
+  roomSettings: {
+    roomName: string;
+    maximumOfUser: number;
+    totalRound: number;
+    isLocked: boolean;
+    password: string;
+  };
+}
+
 const Room = () => {
+  const nav = useNavigate();
   const { roomCode } = useParams();
+  const [roomInfo, setRoomInfo] = useState<RoomType>();
   const [users, setUsers] = useState<any>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const socket = useRef<any>();
@@ -12,14 +32,21 @@ const Room = () => {
     setUsers((prev: any) => [...prev, user]);
   };
 
-  const addUsers = (user: any) => {
+  const initUsers = (user: any) => {
     setUsers([...user]);
+  };
+
+  const loadRoomInfo = (roomInfo: RoomType) => {
+    setRoomInfo(roomInfo);
+  };
+
+  const exitRoom = () => {
   };
 
   useEffect(() => {
     if (!roomCode) return;
     if (socket.current) return;
-    socket.current = Socket.join({ addUser, addUsers });
+    socket.current = Socket.join({ addUser, initUsers, loadRoomInfo });
     socket.current.joinRoom(roomCode.slice(1));
 
     return () => {
@@ -33,7 +60,22 @@ const Room = () => {
     setIsLoading(true);
   }, [users]);
 
-  return isLoading ? <Board /> : <></>;
+  return isLoading ? (
+    <>
+      <div>
+        <div>
+          <button onClick={exitRoom}>◀</button>
+        </div>
+        <div>
+          <text>{roomInfo?.roomSettings.roomName}</text>
+          <text>{roomInfo?.roomSettings.isLocked}</text>
+        </div>
+      </div>
+      <Board />
+    </>
+  ) : (
+    <></>
+  );
 };
 
 export default Room;
