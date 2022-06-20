7b20323fae762307aebb92b01733119ab8337499
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2022-06-20 17:04:35 +0900

    :lipstick: Modal  componenet css 위치 수정

--- client/src/components/common/Modal.style.ts
+++ client/src/components/common/Modal.style.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 export const ModalContainer = styled.div<{ isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  max-width: 500px;
+  max-height: 500px;
+
   border: solid 1px black;
+  border-radius: 10px;
   padding: 10px;
 
   display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
